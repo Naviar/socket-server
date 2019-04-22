8d4c489ec8c52a4636526cf7559b29783507d250
Ivan David Arango Lizarazo <ivan.arango@ibm.com>
2019-04-22 12:00:58 -0500

    backup server recibiendo y emitiendo

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,50 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';//para comunicar xpress con io
+
+import * as socket from '../sockets/sockets';
 
 export default class Server {
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
+    public io:socketIO.Server;//propiedad encargada de emitir eventos y escuchar
+    public httpServer: http.Server;
 
-    constructor() {
-        this.app = express();
+    private constructor() {
+        this.app = express(); 
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+        
+        this.escucharSockets();
+    }
+    public static get instance (){
+        
+        return this._instance || (this._instance = new this());
+
+    }
+
+    private escucharSockets(){
+        
+        console.log('escuchando conexiones - sockets');
+
+        this.io.on('connection', cliente => {
+            
+            console.log('nuevo cliente conectado');
+            // mensajes
+            socket.mensaje(cliente, this.io);
+            // desconectar
+            socket.desconectar(cliente);
+        });
+
+        
     }
 
     start(callback: Function) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port , callback);
     }
 }
